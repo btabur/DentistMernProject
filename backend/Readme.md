3c378bd05b8770a150c757ee06f912fd53adbdf7
Balı Tabur <webtasarim.0323@gmail.com>
2024-06-19 21:55:15 +0300

    backend/connected mongodb

--- backend/Readme.md
+++ backend/Readme.md
@@ -2,4 +2,8 @@
  - npm init -y   komutu ile hazır bir package.json dosyası oluşturulur.
  - express kurulumu: `npm i express` komutunu kullan
  - nodemon:backend deki her değişikliği algılayıp server ı her seferinde baştan başlatmamıza gerek bırakmayan bir modüldür. `npm i nodemon` komutu ile indirdikten sonra package.json dosyasında ` "start": "nodemon server.js"` eklemeyi unutmayın.
- 
+2. aşama
+- mongoose: mongodb ile node.js projesini bağlantısını sağlayan bir modüldür. `npm i mongoose`
+-dotenv: .env dosyasındaki verileri almak için kullanılır. şifre kullanıcı adı gibi `npm i dotenv`
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const mongoose =require('mongoose');
+const dotenv= require('dotenv')
 
 
 
 const app =express()
+const port = 5000;
+dotenv.config();
+
+const connect = async ()=> {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+       console.log("connected mongo");
+    } catch (error) {
+        throw error
+    }
+}
 
 app.get('/',(req,res)=> {
     res.send('hello worrl')
 })
 
-app.listen(5000,()=> {
-    console.log(`server started ${5000}`);
+app.listen(port,()=> {
+    connect()
+    console.log(`server started ${port}`);
 })
